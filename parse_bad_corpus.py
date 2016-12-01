71e1bd304471f60c204ac165a6171eadad149d9c
ngrudzinski <n.grudzinski@siu.edu>
2016-11-30 22:08:11 -0600

    Added script that will parse bad_corpus set and spit out new files in the "neg" directory. *Make sure to create the "neg" directory in the same location as the script before running. Has not been tested otherwise.

---
+++ parse_bad_corpus.py
@@ -0,0 +1,27 @@
+# This is a script that will parse a file named
+# bad_corpus.txt into a training and testing set
+# formatted correctly.
+
+with open("bad_corpus.txt") as f:
+    count = 0
+    file_name = "neg/0000.txt"
+    file_object = open(file_name, "w")
+    for line in f:
+        if line[0] != "#":
+            length = len(line)
+            line = line[:length-1]
+            line_with_no_person = ""
+            if "@person" in line:
+                word_list = line.split()
+                line_with_no_person = " ".join([i for i in word_list if i != "@person" and i != "@person:"])
+            else:
+                line_with_no_person = line
+            if line_with_no_person != '':
+                file_object.write(line_with_no_person + "\n")
+            else:
+                file_object.close()
+                count += 1
+                count_string = str(count)
+                count_length = len(count_string)
+                file_name = "neg/" + "0" * (4 - count_length) + count_string + ".txt"
+                file_object = open(file_name, "w")
