6de7d5059d6d5fd2569f108e83fff0ae979aad89
Mark Ira Goldberg <markiragoldberg@outlook.com>
2016-12-05 12:39:42 -0600

    Make vectorizer Ignore decode errors
    This isn't ideal and could harm our results, but it actually runs now. Figuring out the proper encoding would be better if possible.

--- train_twitter_data.py
+++ train_twitter_data.py
@@ -6,6 +6,7 @@ categories = ['neg', 'pos']
 
 twitter_train = load_files('./twitter_data/twitter_data-train', categories=categories, load_content=True, shuffle=True, random_state=42)
 
-count_vect = CountVectorizer()
+#Ignoring decode errors may harm our results, but at least it works now
+count_vect = CountVectorizer(decode_error='ignore')
 X_train_counts = count_vect.fit_transform(twitter_train.data)
 print(X_train_counts.shape)
