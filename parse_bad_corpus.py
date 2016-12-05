fc0f22842bc83c6a23c7e5c3a6241c756fc4c370
markiragoldberg@outlook.com <markiragoldberg@outlook.com>
2016-12-05 13:10:58 -0600

    Update parse_bad_corpus.py to use regexes

--- parse_bad_corpus.py
+++ parse_bad_corpus.py
@@ -2,20 +2,26 @@
 # bad_corpus.txt into a training and testing set
 # formatted correctly.
 
+import re
+
 with open("bad_corpus.txt") as f:
     count = 0
     file_name = "neg/0000.txt"
     file_object = open(file_name, "w")
+    person_filter = re.compile(r'@person:?')
+    rt_filter = re.compile(r'RT')
+
     for line in f:
         if line[0] != "#":
             length = len(line)
             line = line[:length-1]
-            line_with_no_person = ""
-            if "@person" in line:
-                word_list = line.split()
-                line_with_no_person = " ".join([i for i in word_list if i != "@person" and i != "@person:"])
-            else:
-                line_with_no_person = line
+#            line = rt_filter.sub("", line)
+            line_with_no_person = person_filter.sub("", line)
+#            if "@person" in line:
+#                word_list = line.split()
+#                line_with_no_person = " ".join([i for i in word_list if i != "@person" and i != "@person:"])
+#            else:
+#                line_with_no_person = line
             if line_with_no_person != '':
                 file_object.write(line_with_no_person + "\n")
             else:
