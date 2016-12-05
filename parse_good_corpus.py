175d5e6bfd3949ddb488c691d23b8c28ed799a6d
Mark Ira Goldberg <markiragoldberg@outlook.com>
2016-12-05 12:41:15 -0600

    Strip out emojis using a regex
    I'm not certain this is necessary anymore with changes to the Vectorizer, but it's done. Revert this commit if appropriate.

--- parse_good_corpus.py
+++ parse_good_corpus.py
@@ -2,6 +2,8 @@
 # good_corpus.txt into a training and testing set
 # formatted correctly.
 
+import re
+
 with open("good_corpus.txt") as f:
     count = 0
     file_name = "pos/0000.txt"
@@ -9,22 +11,43 @@ with open("good_corpus.txt") as f:
     k = 0
     j = 0
     file_number = 0
+
+    person_filter = re.compile(r'@person:?')
+    rt_filter = re.compile(r'RT')
+
+    try:
+        # UCS-4
+        emoji_filter = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
+    except re.error:
+        # UCS-2
+        emoji_filter = re.compile(
+            u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
+
+    #Nate, I'm not really sure what j was supposed to do separately from k.
     for line in f:
-        if k < 15000 and (j % 500) == 0:
+        if k < 15000 and (k % 500) == 0:
             line_with_no_generics = line[:len(line)-1]
             if file_number != k // 500:
                 file_number += 1
                 file_object.close()
                 file_name = "pos/" + "0" * (4 - len(str(file_number))) + str(file_number) + ".txt"
                 file_object = open(file_name, "w")
-            if "@person" in line or "RT" in line:
-                word_list = line.split()
-                line_with_no_generics = " ".join([i for i in word_list if i != "@person" and i != "@person:" and i != "RT"])
+
+            # Strips out "RT", "@person", "@person:"
+            # Does this improve the results, Nate?
+            line_with_no_generics = person_filter.sub("", line_with_no_generics)
+            line_with_no_generics = rt_filter.sub("", line_with_no_generics)
+
+            # Strips out the unicode emojis that are causing problems
+            line_with_no_emoji_in_unicode = emoji_filter.sub("", line_with_no_generics.decode('utf-8'))
+            line_with_no_generics = line_with_no_emoji_in_unicode.encode('utf-8')
+
+
+#            if "@person" in line or "RT" in line:
+#                word_list = line.split()
+#                line_with_no_generics = " ".join([i for i in word_list if i != "@person" and i != "@person:" and i != "RT"])
             if line_with_no_generics != '':
                 file_object.write(line_with_no_generics + "\n")
-                j += 1
-                k += 1
-        elif k > 3000:
-            break
+        k += 1
 
     file_object.close()
