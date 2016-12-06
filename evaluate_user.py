ecc3667098f6443694f19bfa84e1b8fae5b14b27
ngrudzinski <n.grudzinski@siu.edu>
2016-12-05 21:46:32 -0600

    Lab2 print statements are clean and the key can be inputted as lowercase or uppercase.

---
+++ evaluate_user.py
@@ -0,0 +1,37 @@
+from sklearn.datasets import load_files
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDClassifier
+
+
+def main():
+
+    categories = ['neg', 'pos']
+
+    twitter_train = load_files('./twitter_data/twitter_data-train', encoding='utf-8', decode_error='ignore',
+                               categories=categories, load_content=True, shuffle=True, random_state=42)
+
+    count_vect = CountVectorizer(decode_error='ignore')
+    text_clf = Pipeline([('vect', CountVectorizer(ngram_range=(1, 1))),
+                         ('tfidf', TfidfTransformer()),
+                         ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                                               alpha=1e-2, n_iter=5, random_state=42))])
+    text_clf = text_clf.fit(twitter_train.data, twitter_train.target)
+
+    f = open("scrapings.text", "r")
+    neg_count = 0.0
+    pos_count = 0.0
+    for line in f:
+        line_list = [line]
+        predicted = str(text_clf.predict(line_list))
+        category = str(twitter_train.target_names[int(predicted[1])])
+        if category == "neg":
+            neg_count += 1
+        elif category == "pos":
+            pos_count += 1
+    print("Percent Positive: " + str(pos_count / (pos_count + neg_count)))
+    print("Percent Negative: " + str(neg_count / (pos_count + neg_count)))
+
+if __name__ == "__main__":
+    main()
--- scraper.py
+++ scraper.py
@@ -4,29 +4,36 @@ import tweepy
 import json
 from tweepy import OAuthHandler
 
-consumer_key = 'KGrJyI9GdujKcowMfrheB78dR'
-consumer_secret = 'TEa81xn0OZOwebVyNccqQFkY2Qe0qPJbHixz1GZQZlJZ55jY2V'
-access_token = '3021600922-wF6n5jmtjnd6Ip16L3CguHxK4p19OblPXjvJYvF'
-access_secret = 'fVpGcfRkzJB0C0lyRUEMr7lGOhz8j1cBQ8PUyQx4FWzaK'
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+def main():
+    consumer_key = 'KGrJyI9GdujKcowMfrheB78dR'
+    consumer_secret = 'TEa81xn0OZOwebVyNccqQFkY2Qe0qPJbHixz1GZQZlJZ55jY2V'
+    access_token = '3021600922-wF6n5jmtjnd6Ip16L3CguHxK4p19OblPXjvJYvF'
+    access_secret = 'fVpGcfRkzJB0C0lyRUEMr7lGOhz8j1cBQ8PUyQx4FWzaK'
 
-api = tweepy.API(auth)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
 
-# the 10 is an arbitrary value
-# using too much bandwidth puts the Twitter Police on you
-#for status in tweepy.Cursor(api.home_timeline).items(10):
-    # process just one status
-    # I think a status is like a post?
-#    print(status.text)
+    api = tweepy.API(auth)
 
-f = open("scrapings.text", "w+")
-timeline = api.user_timeline(screen_name = "56b3197f775b41a", include_rts = True, count=10)
-for tweet in timeline:
-    print(tweet.text)
-    f.write(tweet.text + "\n")
+    # the 10 is an arbitrary value
+    # using too much bandwidth puts the Twitter Police on you
+    #for status in tweepy.Cursor(api.home_timeline).items(10):
+        # process just one status
+        # I think a status is like a post?
+    #    print(status.text)
+    user_id = raw_input("Enter the id of the user that you want to scrape: ")
+    print(user_id)
+    f = open("scrapings.text", "w+")
+    timeline = api.user_timeline(screen_name = "user_id", include_rts = True, count=10)
+    for tweet in timeline:
+        print(tweet.text)
+        f.write(tweet.text + "\n")
+    f.close()
 
-#for tweet in tweepy.Cursor(timeline).items():
- #   print(tweet.text + "\n")
-  #  f.write(tweet.text + "\n")
+    #for tweet in tweepy.Cursor(timeline).items():
+     #   print(tweet.text + "\n")
+      #  f.write(tweet.text + "\n")
+
+if __name__ == "__main__":
+    main()
